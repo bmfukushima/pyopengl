7d852a8172636af2ab6aa1738a8ab6dd470767be
emmuh <bmfukushima@gmail.com>
2021-01-03 00:47:41 -0800

    safety

--- pyqt5/engines/core/__hierarchy.py
+++ pyqt5/engines/core/__hierarchy.py
@@ -1,21 +1,30 @@
 """
+private     :- / :>
+is  a       -->
+has a       |>
 
-is  a    |-
-has a    |>
+geometry
+    :> Geometry --> object
+    |> Rectangle --> Geometry
+    |> Cube --> Geometry
+material
+    :> AbstractMaterial --> object
+    |> LineMaterial --> MainMaterial
+    |> MainMaterial --> AbstractMaterial
+    |> MeshMaterial --> MainMaterial
+    |> PointMaterial --> MainMaterial
 
-Object3D
-    |- Camera
-    |- Group
-    |- Mesh
-    |    |> Geometry
-    |    |    |- Rectangle
-    |    |    |- Cube
-    |    |> Material
-    |- Scene
-Utils
-    |- Matrix
-    |- ObjectArray
-    |- Uniform
-    |- Attribute
+object
+    :> Object3D --> object
+    |> Camera --> Object3D
+    |> Group --> Object3D
+    |> Mesh --> Object3D
+    |> Scene --> Object3D
+utils
+    |> Attribute
+    |> Matrix
+    |> ObjectArray
+    |> Uniform
+    |> Utils
 
 """
